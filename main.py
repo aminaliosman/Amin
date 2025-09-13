237088052b3ff6ea33b248501015864ab4c5c92f
aminaliosman <aminaliosmanov@gmail.com>
2025-09-13 11:31:09 +0400

    amin

---
+++ main.py
@@ -0,0 +1,24 @@
+storage = {
+    'cap' : 18,# 5 manat
+    'trousers' : 80, # 30 manat
+    'shirt' : 65 # 15 manat
+}
+
+price = {
+    'cap_price' : 5,
+    'trousers_price' : 30,
+    'shirt_price': 15
+}
+
+user = input("Enter the clothes ")
+a = int(input("How many you want take "))
+def b():
+if storage['cap']  - 1 and  balance - price['cap_price']:
+    balance = 200
+    balance - price['cap_price'] or price['trousers_price'] or price['shirt_price']
+    print(f'it is your {balance}')
+elif
+
+b(a)
+
+
