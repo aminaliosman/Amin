602d765724d139d7b98f5bf6a8f525133310d755
aminaliosman <aminaliosmanov@gmail.com>
2025-09-14 10:06:34 +0400

    aaa

--- main.py
+++ main.py
@@ -6,18 +6,28 @@ storage = {
 
 balance = 200
 
-print(f"Bizim balansda: {balance} manat var.")
-print("Bizde cap 5 ₼ , trousers 30 ₼ ve shirt 15 ₼")
+print(f"Bizdaa balancda: {balance} manat var.")
+print("Mağazamızda cap (5 manat), trousers (30 manat), və shirt (15 manat) var.")
 
-a = int(input("Siz ne isteyirsiniz? "))
+customer_choice = input("Nə almaq istəyirsiniz? ")
 
-
-if a == storage['cap']:
-    balance -= storage['cap']
-    print('Siz cap aldiz')
-elif a == storage['trousers']:
-    balance -= storage['trousers']
-    print('Siz trouser aldiz')
-elif a == storage['shirt']:
-    balance -= storage['shirt']
-    print('Siz shirt aldiz')
+if customer_choice == 'cap':
+    if balance >= storage['cap']:
+        balance -= storage['cap']
+        print(f"Siz bir kepka aldınız. Yeni balansınız: {balance} manat.")
+    else:
+        print("Balansınızda kepka almaq üçün kifayət qədər vəsait yoxdur.")
+elif customer_choice == 'trousers':
+    if balance >= storage['trousers']:
+        balance -= storage['trousers']
+        print(f"Siz bir şalvar aldınız. Yeni balansınız: {balance} manat.")
+    else:
+        print("Balansınızda şalvar almaq üçün kifayət qədər vəsait yoxdur.")
+elif customer_choice == 'shirt':
+    if balance >= storage['shirt']:
+        balance -= storage['shirt']
+        print(f"Siz bir köynək aldınız. Yeni balansınız: {balance} manat.")
+    else:
+        print("Balansınızda köynək almaq üçün kifayət qədər vəsait yoxdur.")
+else:
+    print("Üzr istəyirik, belə bir məhsul mövcud deyil.")
